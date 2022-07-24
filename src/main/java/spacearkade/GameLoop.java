dc4a94758a6dc5379760ec2a8b83baf6260d8ab2
yuhsin7676 <yushin7676@gmail.com>
2022-07-24 12:44:27 +0300

    refactor: code has been moved from GameLoop to Global. Sending message works

--- src/main/java/spacearkade/GameLoop.java
+++ src/main/java/spacearkade/GameLoop.java
@@ -3,7 +3,6 @@ package spacearkade;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.utils.Array;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -24,32 +23,15 @@ public class GameLoop extends ApplicationAdapter{
     @Override
     public void create() {
         socketHandler.setConnectListener(session -> {
-            // Перекинуть в Global
-            Player player = new Player();
-            Global.mapPlayer.put(session.getId(), player);
-            for(Map.Entry<Integer, World> entry : Global.mapWorld.entrySet()){
-                int numbPlayer = entry.getValue().addPlayer(player);
-                if(numbPlayer != 0){
-                    player.worldNumber = entry.getKey();
-                    player.worldPointer = entry.getValue();
-                    events.add("Player add to world: " + entry.getKey() + ", and get number: " + player.playerNumber);
-                    return;
-                }
-            }
-            int worldId = Global.createWorld();
-            Global.mapWorld.get(worldId).addPlayer(player);
-            player.worldNumber = worldId;
-            player.worldPointer = Global.mapWorld.get(worldId);
-            events.add("Player add to world: " + worldId + ", and get number: " + player.playerNumber);
+            Player player = Global.addPlayerToWorld(session.getId());
+            events.add("Player add to world: " + player.worldNumber + ", and get number: " + player.playerNumber);
         });
         socketHandler.setDisconnectListener(session -> {
+            Global.removePlayer(session.getId());
             events.add(session.getId() + " just disconnect");
-            World world = Global.mapPlayer.get(session.getId()).worldPointer;
-            if(world.player1 == null && world.player2 == null)
-                Global.mapWorld.remove(world);
-            Global.mapPlayer.remove(session.getId());
         });
         socketHandler.setMessageListener(((session, message) -> {
+            
             events.add(session.getId() + " said " + message);
         }));
     }
@@ -64,7 +46,7 @@ public class GameLoop extends ApplicationAdapter{
                     session.sendMessage(new TextMessage("yes"));
                 else
                     session.sendMessage(new TextMessage("no"));
-                for (String event : events)
+                for(String event : events)
                     session.sendMessage(new TextMessage(event));
             }
             catch(IOException e){
--- src/main/java/spacearkade/Global.java
+++ src/main/java/spacearkade/Global.java
@@ -19,5 +19,34 @@ public class Global {
         Global.primary++;
         return worldId;
     };
+    
+    public static Player addPlayerToWorld(String sessionId){
+        Player player = new Player();
+        Global.mapPlayer.put(sessionId, player);
+        for(Map.Entry<Integer, World> entry : Global.mapWorld.entrySet()){
+            int numbPlayer = entry.getValue().addPlayer(player);
+            if(numbPlayer != 0){
+                player.worldNumber = entry.getKey();
+                player.worldPointer = entry.getValue();
+                return player;
+            }
+        }
+        int worldId = Global.createWorld();
+        Global.mapWorld.get(worldId).addPlayer(player);
+        player.worldNumber = worldId;
+        player.worldPointer = Global.mapWorld.get(worldId);
+        return player;
+    }
+    
+    public static void removePlayer(String sessionId){
+        Player player = Global.mapPlayer.remove(sessionId);
+        World world = player.worldPointer;
+        if(player.playerNumber == 1)
+            world.removePlayer1();
+        else if(player.playerNumber == 2)
+            world.removePlayer2();
+        if(world.player1 == null && world.player2 == null)
+            Global.mapWorld.remove(world);
+    }
             
 }
